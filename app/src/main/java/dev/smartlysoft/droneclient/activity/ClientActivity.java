798a9bf9b39299529c319193ea2f717402ee7de6
cgardev <cgardev@gmail.com>
2017-03-11 20:08:12 +0100

    JSON RCV

--- app/src/main/java/dev/smartlysoft/droneclient/activity/ClientActivity.java
+++ app/src/main/java/dev/smartlysoft/droneclient/activity/ClientActivity.java
@@ -1,13 +1,26 @@
 package dev.smartlysoft.droneclient.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.DataInputStream;
@@ -15,6 +28,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import dev.smartlysoft.droneclient.R;
 import dev.smartlysoft.droneclient.network.JSONReciver;
@@ -24,7 +39,12 @@ public class ClientActivity extends AppCompatActivity {
 
     //UI ELEMENTS
     Button connectBtn;
-    EditText statusTxt;
+    ListView wifiList;
+
+    //CORE ELEEMENTS
+    ArrayList wifiArrayList = new ArrayList();
+    ArrayAdapter<String> arrayAdapter;
+    Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,36 +52,49 @@ public class ClientActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_client);
 
+
+
         //Load UI;
         connectBtn = (Button)findViewById(R.id.ConnectBtn);
-        statusTxt = (EditText)findViewById(R.id.StatusTxt);
+        wifiList = (ListView)findViewById(R.id.WifiList);
+
+        wifiArrayList.add("Empty");
 
         connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new AsyncTcpConnection().execute(1);
+                new AsyncTcpConnection().execute();
             }
         });
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, wifiArrayList);
+        wifiList.setAdapter(arrayAdapter);
     }
 
+    public void invalidate(){
+        wifiList.invalidateViews();
+    }
 
-    private class AsyncTcpConnection extends AsyncTask<Integer,Void,Void>{
-        private Socket client;
-        private DataInputStream in;
-        private DataOutputStream out;
-        private int lastAction;
-        private String exText;
+    private class AsyncTcpConnection extends AsyncTask<Void,Void,Void>{
+
+        private String data;
+        private JSONObject json = new JSONObject();
+
 
         @Override
-        protected Void doInBackground(Integer... params) {
+        protected Void doInBackground(Void... params) {
 
             JSONReciver rcv = new JSONReciver();
             try {
-                rcv.recive();
+                json = rcv.recive();
+
+
             } catch (Exception e) {
-                e.printStackTrace();
+                wifiArrayList.clear();
+                wifiArrayList.add("Net Error");
+
             }
-            
+
             return null;
         }
 
@@ -73,8 +106,14 @@ public class ClientActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-
-            statusTxt.setText("Action:" + lastAction);
+            wifiArrayList.clear();
+            Iterator x = json.keys();
+            while (x.hasNext()){
+                String key = (String) x.next();
+                    wifiArrayList.add(key);
+            }
+            Log.d("si", wifiArrayList.toString());
+            invalidate();
         }
 
         @Override
@@ -90,12 +129,8 @@ public class ClientActivity extends AppCompatActivity {
         @Override
         protected void onCancelled() {
             super.onCancelled();
-            statusTxt.setText("Cancelled: " + exText);
         }
     }
-
-
-
 
 
 }
